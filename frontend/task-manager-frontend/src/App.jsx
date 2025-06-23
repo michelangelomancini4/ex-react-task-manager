f3d2b51bf26ecaaa811d5ae408c8d082be413352
michelangelomancini <michelangelomancini8@gmail.com>
2025-06-23 12:57:29 +0200

    Milestone 2 completed-created context, .env and initial task fetch

--- frontend/task-manager-frontend/src/App.jsx
+++ frontend/task-manager-frontend/src/App.jsx
@@ -1,19 +1,26 @@
 import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+
+import { GlobalProvider } from './context/GlobalContext';
+
 import TaskList from './pages/TaskList';
 import AddTask from './pages/AddTask';
 
 function App() {
   return (
-    <BrowserRouter>
-      <nav>
-        <NavLink to="/">Task List</NavLink>
-        <NavLink to="/add">Aggiungi Task</NavLink>
-      </nav>
-      <Routes>
-        <Route path="/" element={<TaskList />} />
-        <Route path="/add" element={<AddTask />} />
-      </Routes>
-    </BrowserRouter>
+
+    <GlobalProvider>
+      <BrowserRouter>
+        <nav>
+          <NavLink to="/">Lista delle Task</NavLink>
+          <NavLink to="/add">Aggiungi Task</NavLink>
+        </nav>
+        <Routes>
+          <Route path="/" element={<TaskList />} />
+          <Route path="/add" element={<AddTask />} />
+        </Routes>
+      </BrowserRouter>
+    </GlobalProvider>
+
   );
 }
 
